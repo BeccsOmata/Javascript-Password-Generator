33f68ccf9c785b08d87e8e8d8aeec5e0689c8178
B Girl Omata <bgirlomata@gmail.com>
2020-09-28 16:26:53 -0400

    Added prompts

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,3 +13,17 @@ generatePassword.addEventListener('click', () => {
         pwLength = prompt('Choice must only be a numeric value between 8 & 128');
     }
     if (!pwLength) return;
+    //Prompt for additional query filters
+let upper = confirm('Select OK if the password should have uppercase letters');
+let lower = confirm('Select OK if the password should have lowercase letters');
+let numbers = confirm('Select OK if the password should contain numbers');
+let specials = confirm('Select OK if the password should contain special characters');
+let passwordString = '';
+    sourceArray = pwGenerateFlex(upper, lower, numbers, specials);
+    for(let i = 0; i < pwLength; ++i) {
+        randomNum = Math.floor(Math.random() * Math.floor(sourceArray.length));
+let character = String.fromCharCode(sourceArray[randomNum]);
+    passwordString += character;
+}
+    password.innerHTML = passwordString;
+});
